2db9bf002fac3398f64365d1b53f04f071793ac7
Kazi Asef Kabir <asefkabir97@gmail.com>
2025-09-17 20:26:34 +0600

    email activation

--- phi_mart/urls.py
+++ phi_mart/urls.py
@@ -42,6 +42,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', api_root_view),
+    path('',include('users.urls')),
     path('api/v1/', include('api.urls'),name='api-root'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
